ea54a37176c2201b8e1c75c650063d2951bcada0
acsbendi <acsbendi28@gmail.com>
2019-04-08 01:22:10 +0200

    Implemented rebuild-all.sh

---
+++ rebuild-all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+readonly CURRENT_PATH=$(dirname "$(readlink -f "$0")")
+abis=(x86 armeabi-v7a arm64-v8a x86_64)
+for abi in ${abis[@]} ; do
+    pushd ${CURRENT_PATH}/${abi}
+    gnome-terminal --tab -e "/bin/bash -c '${CURRENT_PATH}/rebuild.sh; exec /bin/bash -i'" # The terminal should not be closed after the script finished running.
+    popd
+done
