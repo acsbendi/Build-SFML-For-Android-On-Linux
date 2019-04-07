47d6bcac9828778798d072aba6ca8fd7247daf83
acsbendi <acsbendi28@gmail.com>
2019-04-07 02:18:56 +0200

    Fixed and improved (better logging, removing zip) download-ndk-and-build-sfml.sh

--- download-ndk-and-build-sfml.sh
+++ download-ndk-and-build-sfml.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./check_requirements.sh
+./check-requirements.sh
 if [[ $? != 0 ]]; then
         exit 1
 fi
@@ -9,14 +9,16 @@ readonly LATEST_NDK_VERSION=19c
 readonly ANDROID_ZIP_NAME=android-ndk-r${LATEST_NDK_VERSION}-linux-x86_64.zip
 
 if [[ $# > 0 ]]; then
-    readonly NDK_DOWNLOAD_PATH=%1
+    readonly NDK_DOWNLOAD_PATH=$1
 else
     readonly CURRENT_PATH=$(dirname "$(readlink -f "$0")")
     readonly NDK_DOWNLOAD_PATH=${CURRENT_PATH}
 fi
 
-wget --directory-prefix ${NDK_DOWNLOAD_PATH} https://dl.google.com/android/repository/${ANDROID_ZIP_NAME}
-
-unzip ${NDK_DOWNLOAD_PATH}/${ANDROID_ZIP_NAME} -d ${NDK_DOWNLOAD_PATH}
+echo "Downloading Android NDK..."
+wget --no-verbose --directory-prefix ${NDK_DOWNLOAD_PATH} https://dl.google.com/android/repository/${ANDROID_ZIP_NAME}
+echo "Unzipping Android NDK..."
+unzip -q -d ${NDK_DOWNLOAD_PATH} ${NDK_DOWNLOAD_PATH}/${ANDROID_ZIP_NAME}
+rm -rf ${NDK_DOWNLOAD_PATH}/${ANDROID_ZIP_NAME}
 
 ./build-sfml.bat ${NDK_DOWNLOAD_PATH}/android-ndk-r${LATEST_NDK_VERSION}
