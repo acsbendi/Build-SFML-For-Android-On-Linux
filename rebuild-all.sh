f66f02740349ede3b2906570d9f1b76777563e2f
Bendegúz Ács <30595431+acsbendi@users.noreply.github.com>
2019-04-08 19:30:32 +0200

    Removed failing 64-bit ABIs from rebuild-all.sh

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 readonly CURRENT_PATH=$(dirname "$(readlink -f "$0")")
-abis=(x86 armeabi-v7a arm64-v8a x86_64)
+
+# 64 bit abis (arm64-v8a and x86_64) currently fail due to not finding OpenAL, so they are not included
+abis=(x86 armeabi-v7a)
 for abi in ${abis[@]} ; do
     pushd ${CURRENT_PATH}/${abi}
     gnome-terminal --tab -e "/bin/bash -c '${CURRENT_PATH}/${abi}/rebuild.sh; exec /bin/bash -i'" # The terminal should not be closed after the script finished running.
