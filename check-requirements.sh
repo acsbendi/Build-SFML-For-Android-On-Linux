95db663e033466c5584077ddfe935b3a3b44586e
acsbendi <acsbendi28@gmail.com>
2019-04-08 02:30:58 +0200

    Implemented checking minimum CMake version as a requirement.

--- check-requirements.sh
+++ check-requirements.sh
@@ -8,3 +8,37 @@ for requirement in ${requirements[@]} ; do
             exit 1
     fi
 done
+
+bad_cmake_version(){
+    echo "Minimum required CMake version is ${REQUIRED_CMAKE_MAJOR}.${REQUIRED_CMAKE_MINOR}.${REQUIRED_CMAKE_PATCH}"
+    echo "You only have ${CMAKE_VERSION}"
+    echo "Please update your CMake"
+    exit 1
+}
+
+readonly REQUIRED_CMAKE_MAJOR=3
+readonly REQUIRED_CMAKE_MINOR=7
+readonly REQUIRED_CMAKE_PATCH=2
+
+readonly CMAKE_VERSION=$(cmake --version | head -n 1 | sed 's/cmake version //g')
+readonly CMAKE_MAJOR=$(echo ${CMAKE_VERSION} | sed -r 's/^([0-9]+).*$/\1/')
+readonly CMAKE_MINOR=$(echo ${CMAKE_VERSION} | sed -r 's/^[0-9]+\.([0-9]+).*$/\1/')
+readonly CMAKE_PATCH=$(echo ${CMAKE_VERSION} | sed -r 's/^[0-9]+\.[0-9]+\.([0-9]+).*$/\1/')
+
+if [[ ${CMAKE_MAJOR} < ${REQUIRED_CMAKE_MAJOR} ]] ; then
+    bad_cmake_version
+elif [[ ${CMAKE_MAJOR} > ${REQUIRED_CMAKE_MAJOR} ]] ; then
+    exit 0
+fi
+
+if [[ ${CMAKE_MINOR} < ${REQUIRED_CMAKE_MINOR} ]] ; then
+    bad_cmake_version
+elif [[ ${CMAKE_MINOR} > ${REQUIRED_CMAKE_MINOR} ]] ; then
+    exit 0
+fi
+
+if [[ ${CMAKE_PATCH} < ${REQUIRED_CMAKE_PATCH} ]] ; then
+    bad_cmake_version
+else
+    exit 0
+fi
